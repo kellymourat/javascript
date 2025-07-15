856333af87d124a984601741f5c4aa3b1702b33d
kellymourat <kellymoura.pt@gmail.com>
2025-07-15 16:32:24 +0100

    add JavaScript exercise / first script test

---
+++ exercicios/ex001/firstScript.js
@@ -0,0 +1,7 @@
+// Em JavaScript, alert, confirm e prompt são funções que exibem caixas de diálogo na interface do usuário para interagir com o usuário. alert mostra uma mensagem informativa, confirm solicita confirmação do usuário com "OK" e "Cancelar", e prompt permite ao usuário inserir dados em um campo de texto.
+
+alert("Olá, Mundo!");
+window.alert("Minha primeira mensagem");
+window.confirm("Está gostando de JS");
+window.prompt("Qual é o seu nome ?");
+console.log("End of the test! ");
