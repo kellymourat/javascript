6c778750c2e7dacbc1249447145bf9ba9116abc4
kellymourat <kellymoura.pt@gmail.com>
2025-07-19 10:49:55 +0100

    Update StringManipulation.js

--- ModuloB-Comandos-basicos-JS/Aula6-Tratamento-De-Dados/StringManipulation.js
+++ ModuloB-Comandos-basicos-JS/Aula6-Tratamento-De-Dados/StringManipulation.js
@@ -12,8 +12,9 @@
 
 // .includes(), .startsWith(), .endsWith()
 
-const nome = window.prompt("Qual é o seu nome ?");
-document.writeln(`Seu nome tem ${nome.length} letras`);
-document.writeln(`Olá, ${nome}! Seu nome tem ${nome.length} letras.`);
-document.writeln(`Seu nome em maiúscula é ${nome.toUpperCase()}`);
-document.writeln(`Seu nome em maiúscula é ${nome.toLocaleLowerCase()}`);
+const nome = prompt("Qual é o seu nome?");
+
+console.log(`Olá, ${nome}!`);
+console.log(`Seu nome tem ${nome.length} letras.`);
+console.log(`Seu nome em maiúsculas é: ${nome.toUpperCase()}.`);
+console.log(`Seu nome em minúsculas é: ${nome.toLowerCase()}.`);
