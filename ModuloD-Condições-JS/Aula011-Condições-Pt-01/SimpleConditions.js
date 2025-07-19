571e9c49cecd1f0c059ef2aeb5b625bfb6736bf1
kellymourat <kellymoura.pt@gmail.com>
2025-07-19 10:35:06 +0100

    feat: add simple condition exercise using if statement

---
+++ ModuloD-Condições-JS/Aula011-Condições-Pt-01/SimpleConditions.js
@@ -0,0 +1,7 @@
+// Condição Simples
+const vel = 30.5;
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if (vel > 60) { 
+  console.log(`Você ultrapassou a velocidade permitida. Multado!`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
