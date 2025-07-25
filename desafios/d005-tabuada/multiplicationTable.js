872057d35f158af5112d2269713fe554ad54a5c0
kellymourat <kellymoura.pt@gmail.com>
2025-07-25 16:18:28 +0100

    feat: add multiplication table exercise

---
+++ desafios/d005-tabuada/multiplicationTable.js
@@ -0,0 +1,15 @@
+function tabuada() {
+  let num = document.getElementById("txtn");
+  let tab = document.getElementById("seltab");
+  if (num.value.length == 0) {
+    window.alert("porfavor, digite a tabuada");
+  } else {
+    let n = Number(num.value);
+    let c = 1 
+    while (c <= 10){
+
+      
+    }
+  }
+
+}
